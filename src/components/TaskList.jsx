e251c475797650468c4c1cda6948eaae08d2d99b
Thayse Garcia <thaysegarcia@outlook.com>
2026-03-21 15:35:03 -0300

    edição inline

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function TaskList({tasks, toggleTask, editTask, deleteTask}) {
+  const [editingId, setEditingId] = useState(null);
+  const [editText, setEditText] = useState("");
 
   const handleDelete = (e, id) => {
     e.stopPropagation();
@@ -7,8 +11,8 @@ export default function TaskList({tasks, toggleTask, editTask, deleteTask}) {
 
   const handleEdit = (e, task) => {
     e.stopPropagation();
-    const newText = prompt("Editar tarefa:", task.description);
-    if (newText) editTask(task.id, newText);
+    setEditText(task.description);
+    setEditingId(task.id);
   };
    
   return (
@@ -16,9 +20,25 @@ export default function TaskList({tasks, toggleTask, editTask, deleteTask}) {
       {tasks.map(task => ( 
         <div key={task.id}>
           <li className="cursor-pointer flex justify-between" onClick={() => toggleTask(task.id)}>
-            {task.active 
-              ? task.description
-              : <span className="line-through">{task.description}</span>}
+            {editingId === task.id ? (
+              <input 
+                value={editText}
+                autoFocus
+                onChange={e => setEditText(e.target.value)}
+                onClick={e => e.stopPropagation()}
+                onKeyDown={e => {
+                  if (e.key === 'Enter') {
+                    editTask(task.id, editText);
+                    setEditingId(null);
+                  }
+                }}
+                onBlur={() => {
+                  editTask(task.id, editText);
+                  setEditingId(null);
+                }}/>
+            ) : (
+              task.active ? task.description : <span className="line-through">{task.description}</span>
+            )}
 
             <div className="flex">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-pencil-icon lucide-pencil cursor-pointer w-4" onClick={(e) => handleEdit(e, task)}><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>
